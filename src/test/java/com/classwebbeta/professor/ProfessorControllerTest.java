be129e0a4c27b2d7628f97420ff4c81a6670cf0c
SeraFM <cs04555@uoi.gr>
2022-05-09 15:31:49 +0300

    update

--- src/test/java/com/classwebbeta/professor/ProfessorControllerTest.java
+++ src/test/java/com/classwebbeta/professor/ProfessorControllerTest.java
@@ -1,11 +1,63 @@
 package com.classwebbeta.professor;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProfessorControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	ProfessorController professorController;
+
+	@BeforeEach
+    public void setup() {
+		mockMvc = MockMvcBuilders
+          .webAppContextSetup(context)
+          .build();
+    }
+
+    @Test
+    void testProfessorControllerIsNotNull(){
+        assertNotNull(professorController);
+    }
+
+    @Test
+	void testMockMvcIsNotNull() {
+		assertNotNull(mockMvc);
+	}
+
     @Test
     void testGetAboutPage() {
-
+        
     }
 
     @Test
@@ -19,8 +71,8 @@ public class ProfessorControllerTest {
     }
 
     @Test
-    void testGetLoginPage() {
-
+    void testGetLoginPage() throws Exception {
+        mockMvc.perform(get("/login")).andExpect(status().isOk()).andExpect(model().attributeExists("loginRequest")).andExpect(view().name("login"));
     }
 
     @Test
